7717e913396d828a8a63023fe977a14d31daacd3
Will <WTS012201@gmail.com>
2022-12-17 21:42:27 -0600

    dual monitor wm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,23 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar side 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
+
+## Terminate already running bar instances
+#killall -q polybar
+#
+## Wait until the processes have been shut down
+#while pgrep -x polybar >/dev/null; do sleep 1; done
+#
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#      MONITOR=$m polybar --reload main &
+#      MONITOR=$m polybar --reload side &
+#  done
+#else
+#  polybar --reload example &
+#fi
+
