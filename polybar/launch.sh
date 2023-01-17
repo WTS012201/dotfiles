3bb88d0cbbdf7c7d2e05a7fed2d2906498d1a067
Will <WTS012201@gmail.com>
2023-01-16 20:32:33 -0600

    change display

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -14,18 +14,3 @@ polybar side2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
 
-## Terminate already running bar instances
-#killall -q polybar
-#
-## Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
-#
-#if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#      MONITOR=$m polybar --reload main &
-#      MONITOR=$m polybar --reload side &
-#  done
-#else
-#  polybar --reload example &
-#fi
-
