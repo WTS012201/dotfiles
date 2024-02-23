d6dc138979b59db6821fa17301bd4a8484a3e54f
Will <WTS012201@gmail.com>
2024-02-22 20:25:19 -0600

    updated cpp scm file for tree-sitter

--- nvim/queries/cpp/cpp-highlights.scm
+++ nvim/queries/cpp/cpp-highlights.scm
@@ -1,4 +1,36 @@
-;; inherits c
+; inherits: c
+
+(parameter_list
+   "(" @opening
+   ")" @closing) @container
+
+(argument_list
+   "(" @opening
+   ")" @closing) @container
+
+(parenthesized_expression
+  "(" @opening
+  ")" @closing) @container
+
+(for_range_loop
+  "(" @opening
+  ")" @closing) @container
+
+(condition_clause
+  "(" @opening
+  ")" @closing) @container
+
+(compound_statement
+  "{" @opening
+  "}" @closing) @container
+
+(initializer_list
+  "{" @opening
+  "}" @closing) @container
+
+(field_declaration_list
+   "{" @opening
+   "}" @closing) @container
 
 (template_parameter_list
   "<" @opening
@@ -11,3 +43,15 @@
 (template_argument_list
   "<" @opening
   ">" @closing) @container
+
+(subscript_argument_list
+  "[" @opening
+  "]" @closing) @container
+
+(array_declarator
+  "[" @opening
+  "]" @closing) @container
+
+(lambda_capture_specifier
+  "[" @opening
+  "]" @closing) @container
